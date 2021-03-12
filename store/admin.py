34b124daf75a38177d522a2cb45b38fc2b10a3be
Dayeon Lee <iamdayeonlee@gmail.com>
2021-03-12 19:38:55 +0900

    Set up models

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
+
+admin.site.register(Tutorial)
+admin.site.register(Tag)
+admin.site.register(Curriculum)
+admin.site.register(CurriculumItem)
